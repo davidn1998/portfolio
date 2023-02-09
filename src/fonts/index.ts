f6ee7f41166f4709aca1b67e21af43e21b63e9e5
David Nwachukwu <davidnw1998@gmail.com>
2023-02-09 18:57:27 +0000

    set display:swap on next fonts

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -5,15 +5,18 @@ export const sourceSansPro = Source_Sans_Pro({
   weight: ["200", "300", "400", "600", "700", "900"],
   subsets: ["latin"],
   variable: "--font-source-sans-pro",
+  display: "swap",
 });
 
 export const jetbrainsMono: NextFontWithVariable = JetBrains_Mono({
   subsets: ["latin"],
   variable: "--font-jetbrains-mono",
+  display: "swap",
 });
 
 export const yellowtail: NextFontWithVariable = Yellowtail({
   weight: ["400"],
   subsets: ["latin"],
   variable: "--font-yellowtail",
+  display: "swap",
 });
