4794a04154d0688cd1d94af93242093d0593c37d
David Nwachukwu <davidnw1998@gmail.com>
2023-02-09 10:10:26 +0000

    edit font optimisation

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -5,7 +5,9 @@ type Props = {};
 export const Portfolio = (props: Props) => {
   return (
     <div className="flex flex-col">
-      <p className="mb-8 font-bold">Checkout some of my product concepts 🚀</p>
+      <p className="mb-8 font-bold">
+        Checkout some of my projects 📁 and concepts 🚀
+      </p>
       <div className="grid w-full gap-8 md:grid-cols-2 lg:grid-cols-3">
         <ProjectCard
           projectName="Example"
