77d1126767321adab3099467e2a1d29eb6362d6a
David Nwachukwu <davidnw1998@gmail.com>
2023-02-14 11:36:02 +0000

    change logo and favicon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,12 @@ export const Footer = (props: Props) => {
     <footer className="container mx-auto flex items-center justify-between border-t-2 border-gray-500 p-4 text-sm opacity-100 transition-opacity duration-500 lg:max-w-5xl">
       <p>David Nwachukwu &copy; 2023</p>
       <Link href="/">
-        <Image src="/DevIcon.png" alt="headphones" width={32} height={32} />
+        <Image
+          src="/portfolio_logo.png"
+          alt="headphones"
+          width={32}
+          height={32}
+        />
       </Link>
     </footer>
   );
