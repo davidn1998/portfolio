7a1db97cc81cabce2640d21bfe4d830e937081f4
David Nwachukwu <davidnw1998@gmail.com>
2023-02-14 13:08:37 +0000

    edit portfolio projects style

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -4,19 +4,20 @@ type Props = {};
 
 export const Portfolio = (props: Props) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-1 flex-col">
       <p className="mb-8 font-bold">
         Checkout some of my projects 📁 and concepts 🚀
       </p>
-      <div className="grid w-full gap-8 md:grid-cols-2 lg:grid-cols-3">
+      <div className="flex flex-col">
         <ProjectCard
           name="Charcutify"
           description="A web app built with Next.js for creating
           and visualizing custom charcuterie
           boards. Also presents visualizations and
           recipes for premade boards."
-          url="https://github.com/davidn1998"
-          cleanURL="github.com/davidn1998/charcutify"
+          githubURL="https://github.com/davidn1998"
+          websiteURL="https://github.com/davidn1998"
+          cleanURL="charcutify.com"
           imgURL="/project_thumbnails/charcutify_thumb.png"
         />
       </div>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { FiGithub } from "react-icons/fi";
 
 type Props = {
   name: string;
   description: string;
-  url: string;
+  githubURL: string;
+  websiteURL: string;
   cleanURL: string;
   imgURL: string;
 };
@@ -11,34 +13,50 @@ type Props = {
 export const ProjectCard = ({
   name,
   description,
-  url,
+  githubURL,
+  websiteURL,
   cleanURL,
   imgURL,
 }: Props) => {
   return (
-    <div className="mb-10 flex flex-col">
-      <a href={url} target="_blank" rel="noreferrer">
+    <div className="relative mb-20 mt-10 flex">
+      <a
+        href={githubURL}
+        target="_blank"
+        rel="noreferrer"
+        className="relative min-h-[400px] w-full lg:w-2/3"
+      >
         <Image
           src={imgURL}
           alt="project"
-          width={500}
-          height={500}
-          className="mb-6 h-60 w-full rounded-xl object-cover shadow-lg hover:animate-pulse dark:shadow-neutral-600"
+          fill
+          // width={1657}
+          // height={1017}
+          className="object-cover opacity-10 shadow-xl grayscale filter transition-all duration-300 lg:opacity-100 lg:hover:filter-none"
         />
       </a>
-      <h3 className="mb-4 font-bold">{name}</h3>
-      <p className="mb-4 text-gray-400">{description}</p>
-      <a
-        className="group transform transition-all duration-300 ease-linear hover:text-gray-400"
-        href={url}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <span className="mr-2">{cleanURL}</span>
-        <span className="inline-block transition-transform duration-300 ease-linear group-hover:translate-x-1">
-          &rarr;
-        </span>
-      </a>
+      <div className="absolute right-0 flex flex-col self-center p-8 lg:w-1/2 lg:p-0 lg:text-right">
+        <h3 className="text-xl font-bold">{name}</h3>
+        <p className="my-8 text-neutral-900 shadow-2xl dark:text-neutral-300 lg:bg-purple-800 lg:p-8 lg:text-white">
+          {description}
+        </p>
+        <div className="flex items-center lg:self-end">
+          <a href={githubURL} target="_blank" rel="noreferrer">
+            <FiGithub className="text-xl transition-all hover:text-purple-800 dark:hover:text-purple-400" />
+          </a>
+          <a
+            className="group ml-4 transform transition-all duration-300 ease-linear hover:text-purple-800 dark:hover:text-purple-400"
+            href={websiteURL}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="mr-2">{cleanURL}</span>
+            <span className="inline-block transition-transform duration-300 ease-linear group-hover:translate-x-1">
+              &rarr;
+            </span>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
